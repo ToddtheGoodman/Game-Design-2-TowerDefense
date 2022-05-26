b214a1bcd5dd91f6093c79b983d0dfec9e80a0bc
ToddtheGoodman <84030598+ToddtheGoodman@users.noreply.github.com>
2022-05-26 13:23:30 -0700

    LERP
    LERP and face target

--- Assets/Scripts/EnemyMover.cs
+++ Assets/Scripts/EnemyMover.cs
@@ -6,6 +6,7 @@ public class EnemyMover : MonoBehaviour
 {
 
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] [Range(0f, 5f)] float enemySpeed = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -21,11 +22,28 @@ public class EnemyMover : MonoBehaviour
 
     IEnumerator MoveEnemy()
     {
-        foreach(Waypoint waypoint in path)
+        foreach(Waypoint waypoint in path) //will keep running until it reaches the last waypoint in the path
         {
-            Debug.Log(waypoint.name);
-            transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(2);
+
+            Vector3 startPosition = transform.position; // A LERP needs a start and end position
+            Vector3 endPosition = waypoint.transform.position;
+
+            float travelPercentage = 0f; // 0 is the start position, 1 is the end position
+
+            transform.LookAt(endPosition);
+
+            while(travelPercentage < 1)
+            {
+                travelPercentage += Time.deltaTime * enemySpeed;
+
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercentage);
+                yield return new WaitForEndOfFrame();
+            }
+
+
+            //Debug.Log(waypoint.name);
+            //transform.position = waypoint.transform.position;
+            //yield return new WaitForSeconds(waitTime);
         }
     }
 
